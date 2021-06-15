53096a7aecd77d892f78557779affa75b59ce212
labmem001 <a70007692@126.com>
2021-06-15 16:08:39 +0800

    vue学习-后端初始化

---
+++ src/main/java/com/summer/springvuedemo/demo/utils/WebConfig.java
@@ -0,0 +1,22 @@
+package com.summer.springvuedemo.demo.utils;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//全局配置类，配置跨域请求
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        /**
+         * 1.访问路径
+         * 2.请求来源
+         * 3.方法
+         * 4.允许携带
+         * 5.最大请求时间
+         */
+        registry.addMapping("/**")
+                .allowedOrigins("HTTP://localhost:8080","null")
+                .allowedMethods("GET","POST","PUT","OPTION","DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
